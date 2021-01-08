55982040fcac6a70988098abd20470fd4fb66375
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-08 10:31:35 -0500

    Updating default to be github
    Fixes #4

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,4 +1,3 @@
-# this is quoted with triple quotes so that names with apostrophes in them will work properly
 __author__ = '''{{ cookiecutter.full_name }}'''
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
