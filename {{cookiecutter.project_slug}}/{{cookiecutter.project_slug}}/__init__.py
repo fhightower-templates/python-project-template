f8da6f98559b90e8066912a4d21ebe27d6e18bd0
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-21 07:59:06 -0500

    Improving __init__.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -6,9 +6,10 @@ except ImportError:
 try:
     __version__ = version('{{ cookiecutter.project_slug }}')
 except PackageNotFoundError:
-    # package is not installed
-    message = 'Unable to find a version number for "{{ cookiecutter.project_slug }}"; This is likely indicates the installation of this library failed. Please try re-installing it.'
+    message = 'Unable to find a version number for "{{ cookiecutter.project_slug }}". This likely means the library was not installed properly. Please try re-installing it.'
     print(message)
 
 __author__ = '''{{ cookiecutter.full_name }}'''
 __email__ = '{{ cookiecutter.email }}'
+
+from {{cookiecutter.project_slug}} import *
