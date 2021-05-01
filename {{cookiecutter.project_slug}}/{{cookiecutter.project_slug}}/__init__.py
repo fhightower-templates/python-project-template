17fe3986056a0968f254563f20af1f92c0a55778
Floyd Hightower <floyd.hightower27@gmail.com>
2021-05-01 16:00:57 -0500

    Simplifying version handling
    Fixes #18

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,15 +1,5 @@
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    from importlib_metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version('{{ cookiecutter.project_slug }}')
-except PackageNotFoundError:
-    message = 'Unable to find a version number for "{{ cookiecutter.project_slug }}". This likely means the library was not installed properly. Please try re-installing it.'
-    print(message)
-
-__author__ = '''{{ cookiecutter.full_name }}'''
-__email__ = '{{ cookiecutter.email }}'
-
 from {{cookiecutter.project_slug}} import *
+
+__version__ = '{{ cookiecutter.version }}'
+__author__ = '{{ cookiecutter.full_name }}'
+__email__ = '{{ cookiecutter.email }}'
